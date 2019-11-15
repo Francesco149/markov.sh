ebae373b534219008bb1c4c03c9b9bf29bebf863
Steffen Hirschmann <Steffen.Hirschmann@ipvs.uni-stuttgart.de>
2019-11-15 14:27:57 +0100

    Extract $2 early in the pipeline. Save one awk call.

--- mrkwords.sh
+++ mrkwords.sh
@@ -8,8 +8,8 @@ key="$3"
 if [ -z "$key" ]; then
   word=$(shuf -n 1 < "$file" | cut -d' ' -f1)
 else
-  word=$(awk -v key="$key" '$1 == key' < "$file" |
-    shuf -n 1 | awk '{ print $2 }') || exit
+  word=$(awk -v key="$key" '$1 == key { print $2 }' < "$file" |
+    shuf -n 1) || exit
   [ -z "$word" ] && exit
 fi
 exec "$0" "$file" "$(( n - 1 ))" "$word"
